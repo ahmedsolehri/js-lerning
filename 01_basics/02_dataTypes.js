06e62ae1bf5fe07ba9fabd1a05ba9d9f98e67c0b
ahmedsolehri <46812884+ahmedsolehri@users.noreply.github.com>
2023-10-03 12:27:00 +0000

    learn datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,10 @@
+"use strict";
+
+ console.log("Ahmed")
+
+let name = "ahmed"
+let age = 18
+let isLoggedIn = false
+
+// null.. when server not send a temprature then value return null.. just use case example
+
