fab8614af6a226b8f472c6207e00e65124f84740
ahmedsolehri <46812884+ahmedsolehri@users.noreply.github.com>
2023-10-03 11:20:42 +0000

    learn about variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 229087
+let accountEmail = "ahmed@mail.com"
+var accountPassword = "229"
+accountCity = "lahore"
+let accountdetail;
+
+// try to change the varibale for check it's possible or not
+// accountId = 445 this will not change because it's constant variable
+
+accountEmail = "a@mail.com"
+accountPassword = "1123"
+accountCity = "multan"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
