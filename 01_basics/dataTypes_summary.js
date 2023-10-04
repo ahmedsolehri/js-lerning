5b8230cd6d382991eb23f46770eb1a1f849f82ca
ahmedsolehri <46812884+ahmedsolehri@users.noreply.github.com>
2023-10-04 07:47:36 +0000

    dataType summary and comparision operator

---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,15 @@
+// Primitive
+//  call by value, they give us copy not acutal value
+
+// 7 Types : String, Number, Boolean, Null, Undefined, Symbol, bigint
+
+
+// Non pPimitive/Peference
+
+// Array, Objects, Functions
+
+// +++++++++++++++++++++
+
+// Stack(primitive) .. copy of data recieve
+
+// heap(Non Primitive) .. reference( if change any value affect in original value) 
