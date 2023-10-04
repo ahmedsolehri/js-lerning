39609f4112ccda0a16422319866aa90a56017e9f
ahmedsolehri <46812884+ahmedsolehri@users.noreply.github.com>
2023-10-04 07:48:07 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,11 @@
+const name = "ahmed"
+const carsTotal = 90
+
+console.log(`My Name is ${name} and i have ${carsTotal} cars ccollection`);
+
+
+const gameName = new String("Solehri");
+
+console.log(gameName[0]);
+
+console.log(gameName.length);
