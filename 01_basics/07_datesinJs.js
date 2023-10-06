c110f31df78342bf8f5f7d1b3e6fe14b2689a944
ahmedsolehri <46812884+ahmedsolehri@users.noreply.github.com>
2023-10-06 12:27:56 +0000

    dateTime

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,15 @@
+// let myDate = new Date()
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toUTCString());
+
+// let myCreateDate = new Date(2023, 11, 23)
+// console.log(myCreateDate.toDateString());
+
+// let myLocaleDate = new Date(2023, 11, 23, 5, 6)
+// console.log(myLocaleDate.toLocaleString());
+
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
