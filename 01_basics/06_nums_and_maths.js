51cd789ed2a691cfff863d161633e9bd8e2a2152
ahmedsolehri <46812884+ahmedsolehri@users.noreply.github.com>
2023-10-06 11:19:24 +0000

    math and nums

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,22 @@
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+
+
+// const otherNumbers = 23.3345
+// console.log(otherNumbers.toPrecision(3));
+
+// const hunsdrands = 1000000
+// console.log(hunsdrands.toLocaleString('en-IN'));
+
+// ++++++++++++++++++ maths +++++++++++
+
+
+console.log(Math);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min)) + min)
+
